4bc7c587cbe2cc2d64b5b7acbd9a5b101d718abe
eastdh <eastdh@gmail.com>
2026-09-17 16:48:17 +0900

    0917 apache tomcat 실습

--- javascript/modules/main.js
+++ javascript/modules/main.js
@@ -1,9 +1,25 @@
-const firstNum = getDom(".input-number-1");
-const secondNum = getDom(".input-number-2");
+import { getDom, onClick, getValue, setText } from "./calculator.js";
+const firstNum = getDom("#input-number-1");
+const secondNum = getDom("#input-number-2");
 
 const addButton = getDom(".sum");
+const subButton = getDom(".sub");
+const mulButton = getDom(".mul");
+const divButton = getDom(".div");
 const resultDOM = getDom(".result");
 onClick(addButton, function () {
   const result = getValue(firstNum) + getValue(secondNum);
+  setText(resultDOM, result);
+});
+onClick(subButton, function () {
+  const result = getValue(firstNum) - getValue(secondNum);
+  setText(resultDOM, result);
+});
+onClick(mulButton, function () {
+  const result = getValue(firstNum) * getValue(secondNum);
+  setText(resultDOM, result);
+});
+onClick(divButton, function () {
+  const result = getValue(firstNum) / getValue(secondNum);
   setText(resultDOM, result);
 });
