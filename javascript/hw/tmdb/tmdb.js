70e099fff9ad02c3c7a1808d57bdd3c54a52697a
eastdh <eastdh@gmail.com>
2026-09-17 15:43:51 +0900

    TMDB API 실습

--- javascript/hw/tmdb/tmdb.js
+++ javascript/hw/tmdb/tmdb.js
@@ -21,21 +21,33 @@
 //   "vote_count": 2705
 // },
 // #endregion
-// #region TMDB 요청 API
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZThiYTU2M2I4MjhhMjI3ZWZhYmFiNjhjYjExNTBmMSIsIm5iZiI6MTc4OTQ1OTMxNC4zMDcwMDAyLCJzdWIiOiI2YWE4ZmI3MjdmZjk2N2I3MTY2MDljZWYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.JsR5SPpW0krRixBv-twQsnqmPa2l94burPc-3ZOE_LM",
-  },
+
+onload = async () => {
+  const movies = await getMovies();
 };
-//
-fetch(
-  "https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=ko-KR&page=1&sort_by=popularity.desc",
-  options,
-)
-  .then((res) => res.json())
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
-// #endregion
+
+/**
+ * TMDB 영화 요청 API
+ * @returns movie data array
+ */
+async function getMovies() {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZThiYTU2M2I4MjhhMjI3ZWZhYmFiNjhjYjExNTBmMSIsIm5iZiI6MTc4OTQ1OTMxNC4zMDcwMDAyLCJzdWIiOiI2YWE4ZmI3MjdmZjk2N2I3MTY2MDljZWYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.JsR5SPpW0krRixBv-twQsnqmPa2l94burPc-3ZOE_LM",
+    },
+  };
+  //
+  try {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=ko-KR&page=1&sort_by=popularity.desc",
+      options,
+    );
+    const movieRawData = await response.json();
+    return movieRawData.results;
+  } catch (err) {
+    console.error("err.message", err.message);
+  }
+}
